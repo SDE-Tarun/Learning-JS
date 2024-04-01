cbd43c6e2d5b9f5946ed88684fcb1fe8b8712546
Tarun kumar <tarunkumar23j@gmail.com>
2024-04-01 15:29:01 +0530

    IIFEs Added

---
+++ IIFEs/script.js
@@ -0,0 +1,17 @@
+// IIFE's - Expression ke andar function ko banao aur sath sath call krdo
+// () - Expression
+//  ( function name(params) {
+    
+// } )
+// () - function Call
+
+(
+    (function print() {
+    console.log("Hello");
+})
+) ();
+
+
+
+
+
